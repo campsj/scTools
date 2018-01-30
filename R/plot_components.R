d4f8691b88b37ff5ed241b51dc1e07daf50015c4
jc203 <jordi.camps@kuleuven.be>
2018-01-30 18:25:16 +0100

    Update parameters

--- R/plot_components.R
+++ R/plot_components.R
@@ -5,15 +5,20 @@
 #' @param PCx Principal component to plot on x axis
 #' @param PCy Principal component to plot on y axis
 #' @param group Variable or gene name
-#' @param folder Name of designated folder to save images
-#' @param alpha Decide the transparency of geom
-#' @param palette Number of palette selected from qualitative series on colorbrewer2.org
+#' @param folder Name of designated folder to save image
+#' @param subfolder Name of designated subfolder to save image
 #' @param gene If TRUE it will plot genes and no variables
+#' @param brewer If TRUE it will use colorpalettes from colorbrewer2.org
+#' @param palette Number of palette selected from qualitative series on colorbrewer2.org
+#' @param hex_codes Manuel entry of HEX color codes to plot
+#' @param point_size Size paramter of geom_point
+#' @param alpha Decide the transparency of geom
 #' @param width Width of image in cm
 #' @param height Height of image in cm
+#' @param units Units to plot image in
 #' @return PCA plot of single-cell dataset with variables or genes as color scale
 #' @export
-plot_components <- function(sce_object, PCx, PCy, group, folder,  subfolder, gene = FALSE, brewer = TRUE, palette = 2, hex_codes, point_size = 3, alpha = 0.8, theme = 18, width = 14, height = 10, units = "cm") {
+plot_components <- function(sce_object, PCx, PCy, group, folder, subfolder, gene = FALSE, brewer = TRUE, palette = 2, hex_codes, point_size = 3, alpha = 0.8, theme = 18, width = 14, height = 10, units = "cm") {
   if (brewer == FALSE) {
     temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
     ggplot(temp, aes(PCa, PCb, col = col)) +
