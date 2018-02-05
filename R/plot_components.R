2f68defd8b58be934453adbfed41119f01f66969
jc203 <jordi.camps@kuleuven.be>
2018-02-05 13:43:46 +0100

    Save function

--- R/plot_components.R
+++ R/plot_components.R
@@ -18,43 +18,80 @@
 #' @param units Units to plot image in
 #' @return PCA plot of single-cell dataset with variables or genes as color scale
 #' @export
-plot_components <- function(sce_object, PCx, PCy, group, folder, subfolder, gene = FALSE, brewer = TRUE, palette = 2, hex_codes, point_size = 3, alpha = 0.8, theme = 18, width = 14, height = 10, units = "cm") {
-  if (dir.exists(paste(folder, sep = "")) == FALSE)  {
-    dir.create(paste(folder, sep =""))
+plot_components <- function(sce_object, PCx, PCy, group, gene = FALSE, save = TRUE, folder, subfolder,  brewer = TRUE, palette = 2, hex_codes, point_size = 3, alpha = 0.8, theme = 18, width = 14, height = 10, units = "cm") {
+  if (save == TRUE) {
+
+    if (dir.exists(paste(folder, sep = "")) == FALSE)  {
+      dir.create(paste(folder, sep =""))
   }
-  if (dir.exists(paste(folder, "/", subfolder, sep = "")) == FALSE)  {
-    dir.create(paste(folder, "/", subfolder, sep =""))
+    if (dir.exists(paste(folder, "/", subfolder, sep = "")) == FALSE)  {
+      dir.create(paste(folder, "/", subfolder, sep =""))
   }
-  if (brewer == FALSE) {
-    temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
-    ggplot(temp, aes(PCa, PCb, col = col)) +
-      geom_point(size = point_size, alpha = alpha) +
-      labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
-      scale_color_manual(values = hex_codes) +
-      theme_bw(base_size = theme) +
-      theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-            legend.title = element_blank()) +
-      ggsave(paste(folder, "/",subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+    if (brewer == FALSE) {
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
+      ggplot(temp, aes(PCa, PCb, col = col)) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
+        scale_color_manual(values = hex_codes) +
+        theme_bw(base_size = theme) +
+        theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+              legend.title = element_blank()) +
+        ggsave(paste(folder, "/",subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
   } else if (gene == TRUE) {
-    temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], gene_name = exprs(sce_object[group])[1, ])
-    ggplot(temp, aes(PCa, PCb, col = gene_name), alpha = 0.8) +
-      geom_point(size = point_size, alpha = alpha) +
-      labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
-      guides(color = guide_colorbar(barwidth = 0.5, barheight = 8, ticks = FALSE)) +
-      scale_color_viridis(option = "viridis") +
-      theme_bw(base_size = theme) +
-      theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-            legend.title = element_text(size = 24, face = "bold")) +
-      ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], gene_name = exprs(sce_object[group])[1, ])
+      ggplot(temp, aes(PCa, PCb, col = gene_name), alpha = 0.8) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
+        guides(color = guide_colorbar(barwidth = 0.5, barheight = 8, ticks = FALSE)) +
+        scale_color_viridis(option = "viridis") +
+        theme_bw(base_size = theme) +
+        theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+              legend.title = element_text(size = 24, face = "bold")) +
+        ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
   } else {
-    temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
-    ggplot(temp, aes(PCa, PCb, col = col), alpha = 0.8) +
-      geom_point(size = point_size, alpha = alpha) +
-      labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
-      scale_color_brewer(type = "qual", palette = palette) +
-      theme_bw(base_size = theme) +
-      theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-            legend.title = element_blank()) +
-      ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
+      ggplot(temp, aes(PCa, PCb, col = col), alpha = 0.8) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) +
+        scale_color_brewer(type = "qual", palette = palette) +
+        theme_bw(base_size = theme) +
+        theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+              legend.title = element_blank()) +
+        ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+    }
+  }
+  else if (save == FALSE) {
+    if (brewer == FALSE) {
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
+      ggplot(temp, aes(PCa, PCb, col = col)) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) #+
+        #scale_color_manual(values = hex_codes) +
+        #theme_bw(base_size = theme) +
+        #theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+        #      legend.title = element_blank()) +
+        #ggsave(paste(folder, "/",subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+  } else if (gene == TRUE) {
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], gene_name = exprs(sce_object[group])[1, ])
+      ggplot(temp, aes(PCa, PCb, col = gene_name), alpha = 0.8) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) #+
+        #guides(color = guide_colorbar(barwidth = 0.5, barheight = 8, ticks = FALSE)) +
+        #scale_color_viridis(option = "viridis") +
+        #theme_bw(base_size = theme) +
+        #theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+        #      legend.title = element_text(size = 24, face = "bold")) +
+        #ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+  } else {
+      temp <- data.frame(PCa = sce_object[[PCx]], PCb = sce_object[[PCy]], col = sce_object[[group]])
+      ggplot(temp, aes(PCa, PCb, col = col), alpha = 0.8) +
+        geom_point(size = point_size, alpha = alpha) +
+        labs(x = paste("Principal component ", PCx, sep = ""), y = paste("Principal compoenent ", PCy, sep = ""), color = paste(group)) #+
+        #scale_color_brewer(type = "qual", palette = palette) +
+        #theme_bw(base_size = theme) +
+        #theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+        #      legend.title = element_blank()) +
+        #ggsave(paste(folder, "/", subfolder, "/", group, "_", PCx, "_", PCy,".tiff", sep=""), width = width, height = height, units = units)
+    }
   }
 }
