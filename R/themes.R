952844e0ae0f12d4fdabe2e7bca21ddf5fbf48c2
campsj <jordicamps203@gmail.com>
2018-05-03 15:38:27 +0200

    Standard theme for plotting

---
+++ R/themes.R
@@ -0,0 +1,11 @@
+scTools_theme_opts <- function()
+{
+  theme(strip.background = element_rect(colour = 'white', fill = 'white')) +
+    theme(panel.border = element_blank()) +
+    theme(axis.line.x = element_line(size=0.25, color="black")) +
+    theme(axis.line.y = element_line(size=0.25, color="black")) +
+    theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank()) +
+    theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank()) +
+    theme(panel.background = element_rect(fill='white')) +
+    theme(legend.key=element_blank())
+}
