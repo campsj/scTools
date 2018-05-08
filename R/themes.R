d4e840f55e3f0cf4cd70f786fdb177d45f4b2403
campsj <jordicamps203@gmail.com>
2018-05-08 16:06:48 +0200

    axis lines adapted

--- R/themes.R
+++ R/themes.R
@@ -1,9 +1,12 @@
+#' Standard plotting theme
+#' @export
+
 scTools_theme_opts <- function()
 {
   theme(strip.background = element_rect(colour = 'white', fill = 'white')) +
     theme(panel.border = element_blank()) +
-    theme(axis.line.x = element_line(size=0.25, color="black")) +
-    theme(axis.line.y = element_line(size=0.25, color="black")) +
+    theme(axis.line.x = element_line(size=1, color="black")) +
+    theme(axis.line.y = element_line(size=1, color="black")) +
     theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank()) +
     theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank()) +
     theme(panel.background = element_rect(fill='white')) +
